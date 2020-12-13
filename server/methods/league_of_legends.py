980f364cd60eb146d28577fc604fd867fa503384
Your Name <you@example.com>
2020-12-14 00:51:56 +0300

    league tested

--- server/methods/league_of_legends.py
+++ server/methods/league_of_legends.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from aiohttp import ClientSession
+import os
 
 
 class LolMatchStats:
@@ -11,54 +12,66 @@ class LolMatchStats:
     heal: int
     vision_score: int
     kda: int
+    role: str
 
-    def __init__(self, json, game_duration):
+    def __init__(self, stats, game_duration, role):
         mins = game_duration / 60
 
-        self.dmg_taken = json['totalDamageTaken'] / mins
-        self.cc_dealt = json['totalTimeCrowdControlDealt'] / mins
-        self.dmg_dealt = json['totalDamageDealtToChampions'] / mins
-        self.minions_killed = json['totalMinionsKilled'] / mins
-        self.heal = json['totalHeal'] / mins
-        self.vision_score = json['visionScore'] / mins
-        self.kda = (json['kills'] + json['assists']) / json['deaths']
+        self.dmg_taken = stats['totalDamageTaken'] / mins
+        self.cc_dealt = stats['totalTimeCrowdControlDealt'] / mins
+        self.dmg_dealt = stats['totalDamageDealtToChampions'] / mins
+        self.minions_killed = stats['totalMinionsKilled'] / mins
+        self.heal = stats['totalHeal'] / mins
+        self.vision_score = stats['visionScore'] / mins
+        if stats['deaths']:
+            self.kda = (stats['kills'] + stats['assists']) / stats['deaths']
+        else:
+            self.kda = 1000
+        self.role = role
 
 
 class LeagueOfLegends:
-    _api_url = ''
-    _summoner_id: int
+    _api_url: str
+    _account_id: int
     stats: List[LolMatchStats]
 
     async def init(self, summoner_id: int = None, summoner_name: str = None):
+        self._api_url = f'https://euw1.api.riotgames.com%s?api_key={os.getenv("LOL_API_KEY")}'
         if summoner_id and not summoner_name:
-            self._summoner_id = summoner_id
+            self._account_id = summoner_id
         elif not summoner_id and summoner_name:
-            self._summoner_id = await self._get_account_id(summoner_name)
+            self._account_id = await self._get_account_id(summoner_name)
+        return self
 
+    async def stats_to_cache(self):
         self.stats = await self._get_games_stats()
 
     async def _get_account_id(self, summoner_name) -> int:
-        url = f'{self._api_url}/lol/summoner/v4/summoners/by-name/{summoner_name}'
-        async with ClientSession as session:
+        url = self._api_url % f'/lol/summoner/v4/summoners/by-name/{summoner_name}'
+        async with ClientSession() as session:
             async with session.get(url) as response:
                 return (await response.json())['accountId']
 
     async def _get_games_stats(self, count=20) -> List[LolMatchStats]:
-        matches_url = f'{self._api_url}/lol/match/v4/matchlists/by-account/{self._summoner_id}'
-        match_url = f'{self._api_url}/lol/match/v4/matches/%d'
-        async with ClientSession as session:
-            game_duration: int
-
-            match_id: list
+        matches_url = self._api_url % f'/lol/match/v4/matchlists/by-account/{self._account_id}'
+        match_url = self._api_url % '/lol/match/v4/matches/%d'
+        async with ClientSession() as session:
+            matches: list
             async with session.get(matches_url) as response:
                 json = await response.json()
-                game_duration = json['gameDuration']
-                match_ids = [i['gameId'] for i in json['matches']][:count]
+                matches = [{'gameId': i['gameId'], 'role': i['role']} for i in json['matches']][:count]
 
             match_stats: List[LolMatchStats] = []
-            for i in match_ids:
-                async with session.get(match_url % i) as response:
+            k = 0
+            for game_obj in matches:
+                async with session.get(match_url % game_obj['gameId']) as response:
+                    k += 1
+                    print(k)
+                    json = await response.json()
+                    participant_id = [i['participantId'] for i in json['participantIdentities'] if i['player']['accountId'] == self._account_id][0]
+
+                    game_duration = json['gameDuration']
                     match_stats.append(
-                        LolMatchStats([i for i in (await response.json())['participants'] if i['participantId'] == self._summoner_id][0], game_duration)
+                        LolMatchStats([i for i in json['participants'] if i['participantId'] == participant_id][0]['stats'], game_duration, game_obj['role'])
                     )
             return match_stats
--- server/methods/test_methods.py
+++ server/methods/test_methods.py
@@ -1,4 +1,23 @@
+from league_of_legends import LeagueOfLegends
+import asyncio
+
+from dotenv import load_dotenv
+from pathlib import Path  # Python 3.6+ only
+env_path = Path(__file__).parent.parent / '.env'
+print(str(env_path))
+load_dotenv(dotenv_path=env_path)
+
+
 class TestMethods:
     @staticmethod
     async def test(user):
         return 1
+
+    @staticmethod
+    async def test_lol(nickname='frakexx'):
+        lol = await LeagueOfLegends().init(summoner_name=nickname)
+        stats = await lol.stats_to_cache()
+
+
+if __name__ == '__main__':
+    asyncio.run(TestMethods.test_lol())
