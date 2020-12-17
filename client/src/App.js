a9ef345e28773ff4875613f298bc131c7c60dd68
Your Name <you@example.com>
2020-12-17 23:36:30 +0300

    refactoring

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
 import {HomePage} from "./pages/home/HomePage";
 import React from "react";
+import {useDispatch} from "react-redux";
+import {fetchUser} from "./actions";
 
 function App() {
+    const dispatch = useDispatch();
+    if (localStorage.getItem('token'))
+        dispatch(fetchUser())
   return (
     <div className="App">
       <HomePage/>
