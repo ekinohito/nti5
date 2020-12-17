d38d76dbc27d86a3f145e5cfa0d92d029694591a
NOTfedos <mistkap@gmail.com>
2020-12-17 21:18:52 +0300

    added maximums of stats dbmodel

---
+++ server/models/maximum_model.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+from db_base import Base
+
+
+class Maximum(Base):
+    __tablename__ = 'maximum'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(150), nullable=False, unique=True)
+    value = Column(String(150), nullable=True, unique=True)
+
+    def __repr__(self):
+        return f"{self.title}: {self.value}"
